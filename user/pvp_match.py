25f07ee2f92dfbfab09b249ccf71e3e2733f410a
altainmunkhg <altainmunkh.g@gmail.com>
2025-10-30 01:17:15 -0400

    Made the movement AI, not very good at the moment, might have to retrain it

--- user/pvp_match.py
+++ user/pvp_match.py
@@ -14,11 +14,12 @@ import pygame
 
 pygame.init()
 
-#my_agent = UserInputAgent()
+my_agent = UserInputAgent()
 #my_agent = SubmittedAgent(file_path="checkpoints/RecurrentPPO_Experiment_3.1/rl_model_3013200_steps")
 my_agent = BasedAgent()
 
 # Input your file path here in SubmittedAgent if you are loading a model:
+#opponent = SubmittedAgent(file_path='checkpoints/Hierarch_Experiment_2_Movement/rl_model_12142434_steps')
 opponent = CustomAgent()
 
 match_time = 999999
