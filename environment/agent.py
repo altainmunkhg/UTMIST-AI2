a22a8dea6ae99cc806e8d6c3b7bd4ad9afeefd8a
Kaden Seto <127336883+kseto06@users.noreply.github.com>
2025-10-30 00:03:13 -0400

    fix: model save on training interruption

--- environment/agent.py
+++ environment/agent.py
@@ -1148,7 +1148,9 @@ def train(
         agent.learn(env, total_timesteps=train_timesteps, verbose=1)
         base_env.on_training_end()
     except KeyboardInterrupt:
-        pass
+        if save_handler is not None:
+            save_handler.agent.update_num_timesteps(save_handler.num_timesteps)
+            save_handler.save_agent()
 
     env.close()
 
